e7ccd276ed0bbd79cb0bf6559c514566a97fd01c
Alex J. Grede <agrede@gmail.com>
2013-06-15 00:15:36 -0400

    Impurities function

---
+++ simulation/impurities.m
@@ -0,0 +1,36 @@
+function NI = impurities(eta,etaV,Imps,T,ImpProps,PC)
+  % 
+  NI = zeros(size(eta,1),length(Imps));
+  for k=1:length(Imps)
+    etaI = nan;
+    iType = 1;
+    iPropT = 'Donors';
+    if (strcmpi(Imps{k}.type,'A'))
+      iType = 0;
+      iPropT = 'Acceptors';
+    endif
+    if (isfield(Imps{k},'energy'))
+       etaI = Imps{k}.*PC.e./(PC.kB.*T);
+    elseif (isfield(Imps{k},'element'))
+      etaI = 0;
+      if (isfield(ImpProps,iPropT))
+        if (isfield(ImpProps.(iPropT),Imps{k}.element))
+          etaI = ImpProps.(iPropT).(Imps{k}.element)./(PC.kB.*T); 
+        endif
+      endif
+    endif
+    if (iType)
+      if (~isnan(etaI))
+        NI(:,k) = (Imps{k}.concentration)./(1+2.*exp(eta+etaI));
+      else
+        NI(:,k) = Imps{k}.concentration;
+      endif
+    else
+      if (~isnan(etaI))
+        NI(:,k) = -(Imps{k}.concentration)./(1+4.*exp(-eta+etaV+etaI));
+      else
+        NI(:,k) = -Imps{k}.concentration;
+      endif
+    endif
+  endfor
+endfunction
