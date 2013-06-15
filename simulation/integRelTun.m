c08ec15a1c40bf0c1f24f6bd53e690881d5d1786
Alex J. Grede <agrede@gmail.com>
2013-06-15 00:15:02 -0400

    Some changes, method is invalid anyways

--- simulation/integRelTun.m
+++ simulation/integRelTun.m
@@ -1,4 +1,4 @@
-function res = integRelTun(y,ind,psi,n,n0,psib,NSR,T,PC)
+function res = integRelTun(y,ind,psi,n,n0,ND,psib,NSR,T,PC)
 
 res = zeros(length(psi),length(psib),length(NSR));
 o2 = ones(1,length(psib));
@@ -6,16 +6,15 @@ o3 = ones(1,length(NSR));
 
 y = y';
 
-phit = PC.kB.*T./PC.e;
-m = psib./phit;
 o1 = ones(1,length(n));
-delNS = permute(n0./n(:,o3).*(1-NSR(o1,:))+NSR(o1,:),[1 3 2]);
+dND = (n+ND)./n0-2;
+delNS = permute(1+dND(:,o3)./(1+NSR(o1,:).*dND(:,o3)),[1 3 2]);
 
 for k=1:length(psi)
   indk = find(ind(k,:));
   if (length(indk)>1)
     o1 = ones(1,length(indk));
-    res(k,:,:) = trapz(y(indk,k.*o2,o3),exp(-1./sqrt(delNS(indk,o2,:).*(m(o1,:,o3)+psi(indk,o2,o3)./phit))));
+    res(k,:,:) = trapz(y(indk,k.*o2,o3),exp(-1./(sqrt(delNS(indk,o2,:).*(1+psi(indk,o2,o3)./psib(o1,:,o3))))));
   endif
 endfor
 endfunction
