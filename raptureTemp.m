53d5592abf7fdcc2a3135c28d54bbd1490ed219f
Alex J. Grede <agrede@gmail.com>
2014-02-09 07:56:57 -0500

    Changing some unit things

--- raptureTemp.m
+++ raptureTemp.m
@@ -8,9 +8,12 @@ B = zeros(3,1);
 [B(1),err] = rpLibGetDouble(lib,'input.group(composition).group(group_b).number(P).current');
 [B(2),err] = rpLibGetDouble(lib,'input.group(composition).group(group_b).number(As).current');
 [B(3),err] = rpLibGetDouble(lib,'input.group(composition).group(group_b).number(Sb).current');
+[tstr,err] = rpLibGetString(lib,'input.group(doping).group(acceptors).number(acceptor_level).current');
+[NA,err]   = rpUnitsConvertDbl(tstr,'m-3');
 
 
 [err] = rpLibPutString(lib,'output.log',sprintf('\nGroup III: %g',sum(A)),1);
 [err] = rpLibPutString(lib,'output.log',sprintf('\nGroup V: %g',sum(B)),1);
+[err] = rpLibPutString(lib,'output.log',sprintf('\nNA: %e',NA),1);
 rpLibResult(lib);
 quit;
